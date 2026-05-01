d2ee99bb8375cd04e51c1a64b5c899c658f8a36d
hagibr <alexandre.hagihara@gmail.com>
2026-05-01 14:47:40 -0300

    Letting my firebase config open

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Renomeie este arquivo para firebase-config.js e adicione suas credenciais do Firebase Console
+// IMPORTANTE: Adicione o arquivo firebase-config.js ao seu .gitignore
+const firebaseConfig = {
+  apiKey: "AIzaSyBHICV7qvVUVwQ1XAD7xV4Q5W3-S440ZgA",
+  authDomain: "bingo-f1a5b.firebaseapp.com",
+  databaseURL: "https://bingo-f1a5b-default-rtdb.firebaseio.com",
+  projectId: "bingo-f1a5b",
+  storageBucket: "bingo-f1a5b.firebasestorage.app",
+  messagingSenderId: "554999191139",
+  appId: "1:554999191139:web:769ddb8eb26b74101eca56",
+  measurementId: "G-JX18SFGC6N"
+};
