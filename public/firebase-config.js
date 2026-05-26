c0081511a12e5add9a83717e8ba27c964170aaf1
hagibr <alexandre.hagihara@gmail.com>
2026-05-26 08:12:42 -0300

    Updating firebase config to new project name

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,12 @@
 // Renomeie este arquivo para firebase-config.js e adicione suas credenciais do Firebase Console
 // IMPORTANTE: Adicione o arquivo firebase-config.js ao seu .gitignore
 const firebaseConfig = {
-  apiKey: "AIzaSyBHICV7qvVUVwQ1XAD7xV4Q5W3-S440ZgA",
-  authDomain: "bingo-f1a5b.web.app",
-  databaseURL: "https://bingo-f1a5b-default-rtdb.firebaseio.com",
-  projectId: "bingo-f1a5b",
-  storageBucket: "bingo-f1a5b.firebasestorage.app",
-  messagingSenderId: "554999191139",
-  appId: "1:554999191139:web:769ddb8eb26b74101eca56",
-  measurementId: "G-JX18SFGC6N"
+  apiKey: "AIzaSyD8QOz-_rFA9Dv4S1LrG97qbCsT6jgtKkw",
+  authDomain: "binguinho.web.app",
+  databaseURL: "https://binguinho-default-rtdb.firebaseio.com",
+  projectId: "binguinho",
+  storageBucket: "binguinho.firebasestorage.app",
+  messagingSenderId: "570768211620",
+  appId: "1:570768211620:web:510dda937e16ff7cb97031",
+  measurementId: "G-34TJQ7HGDV"
 };
